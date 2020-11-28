7b6b9e044e53540c31eadc3485569883e9665709
Natalia Samoilovich <natalia.samoilovich@gmail.com>
2020-11-29 00:37:44 +0300

    Add activity for moview details

--- app/src/main/java/com/samoilovich/courseapp/MainActivity.kt
+++ app/src/main/java/com/samoilovich/courseapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.samoilovich.courseapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        prepareViews()
+    }
+
+    private fun prepareViews() {
+        findViewById<AppCompatButton>(R.id.start_movie_details_button)?.setOnClickListener {
+            startActivity(Intent(this, MovieDetailsActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/samoilovich/courseapp/MovieDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.samoilovich.courseapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
